0f63e733327f67fbd64826b88be702e91ce98310
DenitsaS <denitza97@mail.bg>
2017-11-01 09:53:01 +0200

    fixed the search controller

--- Amazon/src/main/java/com/amazon/controller/SearchController.java
+++ Amazon/src/main/java/com/amazon/controller/SearchController.java
@@ -3,13 +3,13 @@ package com.amazon.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.amazon.dao.SearchDao;
 import com.amazon.model.Book;
+import com.amazon.model.Computer;
 import com.amazon.model.Movie;
 
 @RestController
@@ -32,9 +32,16 @@ public class SearchController {
 ////		return searchDao.getAllBooks().subList((page -1)*pageSize, page*pageSize);
 ////	}
 	
-	@RequestMapping(value = "/books/", method = RequestMethod.GET)
+	@RequestMapping(value = "/books", method = RequestMethod.GET)
 	public List<Book> getAllBooks(){
 		return searchDao.getAllBooks();
 	}
+	
+
+	@RequestMapping(value = "/computers", method = RequestMethod.GET)
+	public List<Computer> getAllComputers(){
+		return searchDao.getAllComputers();
+	}
+	
 	
 }
